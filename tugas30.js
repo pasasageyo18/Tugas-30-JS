16ed37df82b688464f8b722053c36c3f294cab63
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-21 10:44:26 +0700

    first commit

---
+++ tugas30.js
@@ -0,0 +1,7 @@
+function regex(value) {
+  let ambilData = value.match(/[es]/g);
+  console.log(ambilData);
+}
+regex(
+  "Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat"
+);
